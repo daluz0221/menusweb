1c69d48e7fbfb6c8f56cf241f69be278f0a62162
Luis Felipe <daluz0221@gmail.com>
2023-10-29 17:28:11 -0500

    mod imgs

--- src/data/platos.js
+++ src/data/platos.js
@@ -5,7 +5,7 @@ export const Platos = [
         category: 'Tipicos paisas',
         price: 35.000,
         description: 'Delicioso plato antioqueño, con todo lo mejor de la cultura paisa, chicharrón, chorizo, carne molida, huevo, frijoles y arroz, lo mejor para disfrutar',
-        image: './assets/bandeja.webp'
+        image: '../assets/bandeja.webp'
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export const Platos = [
         category: 'Tipicos paisas',
         price: 25.000,
         description: 'La combinación exclusiva para almorzar, trae lo mejor de los antioqueños con un sabor único',
-        image: './assets/fiambre.jpeg'
+        image: '../assets/fiambre.jpeg'
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ export const Platos = [
         category: 'Tipicos paisas',
         price: 5.000,
         description: 'La bebida típica antioqueña que no puede faltar en tus comidas, lo mejor de lo mejor',
-        image: './assets/mazamorra.jpeg'
+        image: '../assets/mazamorra.jpeg'
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ export const Platos = [
         category: 'Tipicos colombianos',
         price: 45.000,
         description: 'Sancocho con mucha sustancia, con extraordinario sabor y tradición. Se acompaña de arroz blanco, tajada de aguacate, hogao y cilantro picado.',
-        image: './assets/sancocho.jpg'
+        image: '../assets/sancocho.jpg'
     },
     {
         id: 5,
@@ -37,7 +37,7 @@ export const Platos = [
         category: 'Tipicos colombianos',
         price: 30.000,
         description: 'El ajiaco  es una sopa típica de Colombia. Contiene pollo en presas o desmenuzado, papa criolla, pastusa y sabanera, mazorca de maíz y opcionalmente alcaparras y crema de leche',
-        image: './assets/ajiaco.webp'
+        image: '../assets/ajiaco.webp'
     },
     {
         id: 6,
@@ -45,7 +45,7 @@ export const Platos = [
         category: 'Tipicos colombianos',
         price: 30.000,
         description: 'El tamal es un alimento mexicano hecho a base de maíz, relleno de diversos ingredientes, cocido en un paquete de hojas vegetales que lo mismo pueden ser de milpa o de maíz, plátano, carrizo, chilaca o papatla',
-        image: './assets/tamal.jpg'
+        image: '../assets/tamal.jpg'
     },
     {
         id: 7,
@@ -53,6 +53,6 @@ export const Platos = [
         category: 'Tipicos colombianos',
         price: 30.000,
         description: 'La lechona es un plato típico de la región Colombiana del Tolima y Huila. Este plato fuerte está compuesto por carne de cerdo, y arvejas principalmente.',
-        image: './assets/lechona.jpg'
+        image: '../assets/lechona.jpg'
     },
 ]
